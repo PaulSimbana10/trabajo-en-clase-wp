200057a1e1e29108fed9471384b56ffc25b87304
Paul Simbaña <dennis-paul10@outlook.es>
2021-06-11 11:31:42 -0500

    Tema responsive

--- footer.php
+++ footer.php
@@ -1,2 +1,4 @@
+<?wp_footer(); ?>
+
 </body>
 </html>
--- front-page.php
+++ front-page.php
@@ -1,2 +1,79 @@
-<? get_header() ?>
-<? get_footer_() ?>
+<?php get_header(); ?>
+<main>
+
+<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+  <div class="md:flex">
+    <div class="md:flex-shrink-0">
+      <img class="h-48 w-full object-cover md:h-full md:w-48" src="<?php echo get_template_directory_uri() ?>/paul.jpg" alt="Foto de perfil">
+    </div>
+    <div class="p-8">
+      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Presentación</div>
+      <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Paul Simbaña</a>
+      <p class="mt-2 text-gray-500">El presente blog es para dar a conocer mis habilidades dentro del mundo de la programación: En mi carrera de analísta en sistemas he aprendido a programar en distitos lenguajes de programación y administrar bases de datos desde un entorno estudiantil.</p>
+    </div>
+  </div>
+</div>
+
+
+<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+  <div class="md:flex">
+    <div class="md:flex-shrink-0">
+      <img class="h-48 w-full object-cover md:h-full md:w-48" src="<?php echo get_template_directory_uri() ?>/java.png" alt="Footo de Java">
+    </div>
+    <div class="p-8">
+      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Conocimientos</div>
+      <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Conocimientos en Java y PHP</a>
+      <p class="mt-2 text-gray-500"> En mis años de estudios en el Intituto Tecnológico Quito he aprendido a programar en lenguajes de programacion muy utiles para el ámbito empresarial como Java y php igual he tenido la oportunidad de ser parte del proyecto de vinculacion del ITQ denominado plataforma E-learning lo cual incrememnto mi experiencia en el campo de WordPress tambien he administrado bases de datos entre otros campos</p>
+    </div>
+  </div>
+</div>
+
+
+<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+  <div class="md:flex">
+    <div class="md:flex-shrink-0">
+      <img class="h-48 w-full object-cover md:h-full md:w-48" src="<?php echo get_template_directory_uri() ?>/ingles.jpg" alt="Foto ingles">
+    </div>
+    <div class="p-8">
+      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Otros Conocimientos</div>
+      <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Conocimientos en Ingles</a>
+      <p class="mt-2 text-gray-500"> Dentro de otros estudios Tengo un conocimento medio en Ingles y Catalá, pase varios años en España en Cataluña lo cual me hizo aprender tal idioma, y en el ámbito del ingles tengo conocimientos por la escuela de idiomas del ITQ, no es mucho pero entiendo e interpreto ideas en ingles, actualmente el ingles es primorial en el mundo de la programacion ya que la mayoria de lenguajes de programacion estan en ingles.</p>
+    </div>
+  </div>
+</div>
+
+
+<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+  <div class="md:flex">
+    <div class="md:flex-shrink-0">
+      <img class="h-48 w-full object-cover md:h-full md:w-48" src="<?php echo get_template_directory_uri() ?>/dota.png" alt="Foto de pasatiempo">
+    </div>
+    <div class="p-8">
+      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Pasatiempos</div>
+      <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Pasatiempos antes y durante la pandemia.</a>
+      <p class="mt-2 text-gray-500"> Mis pasatiempos antes de la pandemia era jugar futbol lo cual me apasiona, se podria decir que era mediamante bueno otro pasatiempo que practicaba era el Ecuaboley lo cual me agrada, durante la pandemia con el hecho de estar encerrados me dedique a jugar Dota 2 y Fornite lo cual lo hago bien otros pasatiempos que tengo es ver videos de programacion de Python lo cual me agrada ya que es un lenguaje que nos sirve en la seguridad informática y es muy importante en la actualidad.</p>
+    </div>
+  </div>
+</div>
+
+
+<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+  <div class="md:flex">
+    <div class="md:flex-shrink-0">
+      <img class="h-48 w-full object-cover md:h-full md:w-48" src="<?php echo get_template_directory_uri() ?>/viajes.jpg" alt="Foto de viajes">
+    </div>
+    <div class="p-8">
+      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Viajes</div>
+      <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Viajes</a>
+      <p class="mt-2 text-gray-500"> En cuanto a viajes he visitados varios lugares pero los que me dieron una experiencia muy satisfactoria son España y Francia, pude visitar la torre Eiffell y en España varios lugares el arco del triunfo la sagrada familia e inclusio los estados de furbol del Barcelona y Madrid, dentro de sudamerica he visitado solo Colombia y Varios lugares de mi amado Ecuador. creo que he tenido agradables experiencias en cuanto a viajes y espero tener muchas experiencias más.</p>
+    </div>
+  </div>
+</div>
+
+
+
+
+</main>
+
+
+<?php get_footer(); ?>
